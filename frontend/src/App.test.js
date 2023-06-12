07cc9307b6ee89fdc54c8229f039971c1bddb65b
learnwithragesh <85107816+learnwithragesh@users.noreply.github.com>
2023-06-12 19:43:00 +0530

    Unit testing (#3)
    * Unit Test Cases and Docker
    
    * Docker Updates
    
    * Validations Added
    
    * Nginx and Docker updates
    
    * Jest configured
    
    * Node JS units
    
    ---------
    
    Co-authored-by: Rageshwaran Rajamani <rageshwaran.rajamani@arcserve.com>

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render } from "@testing-library/react";
+import { Provider } from 'react-redux';
+
+import { store } from './utils/redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('test App components', () =>{
+  const screen = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
